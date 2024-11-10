8962a3153bbc699d926d06265c582feea2d324cb
Austcane <augustinenjala5@gmail.com>
2024-11-10 10:54:53 +0200

    change user to augastine

--- interfaceapp/lib/NewInter.dart
+++ interfaceapp/lib/NewInter.dart
@@ -44,7 +44,7 @@ class _HomeState extends State<Home> {
                         ),
                         SizedBox(width: 10),
                         Text(
-                          "Augastine,",
+                          "Augastine3,",
                           style: GoogleFonts.roboto(
                               color: Colors.white,
                               fontSize: 30,
