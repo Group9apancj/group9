b8511a08f0b4b3c5902983ff8da39a4d18b78224
Phidohphidz <phidohphidz2@gmail.com>
2024-11-09 12:38:15 -1200

    Login and register page added

--- interfaceapp/lib/login.dart
+++ interfaceapp/lib/login.dart
@@ -12,7 +12,7 @@ class _LoginPageState extends State<LoginPage> {
   int h1 = 0;
   int h2 = 0;
   int _selectedValue = 1;
-  int obsec=1;
+  int obsec = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +21,7 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Color(0xffffffff),
         title: Center(
           child: Text(
-            "Create account",
+            "Login",
             style: GoogleFonts.roboto(
                 color: Color(0xff715cf8),
                 fontSize: 42,
@@ -36,10 +36,10 @@ class _LoginPageState extends State<LoginPage> {
             child: Form(
               child: Column(
                 children: [
-                  SizedBox(height: 80),
+                  SizedBox(height: 250),
                   TextFormField(
                     decoration: InputDecoration(
-                      hintText: "Enter your name",
+                      hintText: "Enter name",
                       hintStyle: GoogleFonts.roboto(
                           color: Colors.grey,
                           fontSize: 20,
@@ -59,134 +59,7 @@ class _LoginPageState extends State<LoginPage> {
                   SizedBox(height: 40),
                   TextFormField(
                     decoration: InputDecoration(
-                      hintText: "Enter your age",
-                      hintStyle: GoogleFonts.roboto(
-                          color: Colors.grey,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                      suffixIcon: Icon(
-                        Icons.add,
-                        size: 30,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    style: GoogleFonts.roboto(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                    keyboardType: TextInputType.number,
-                  ),
-                  SizedBox(height: 40),
-                  Container(
-                      width: double.infinity,
-                      child: Column(children: [
-                        Text("Select your gender",
-                          style: GoogleFonts.roboto(
-                              color: Colors.grey,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(children: [
-                              Text(
-                                "Male",
-                                style: GoogleFonts.roboto(
-                                    color: Colors.grey,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Radio(
-                                value: 1,
-                                groupValue: _selectedValue,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedValue = value!;
-                                  });
-                                },
-                              ),
-                            ]),
-                            Row(children: [
-                              Text(
-                                "Female",
-                                style: GoogleFonts.roboto(
-                                    color: Colors.grey,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Radio(
-                                value: 2,
-                                groupValue: _selectedValue,
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedValue = value!;
-                                  });
-                                },
-                              ),
-                            ]),
-                          ],
-                        ),
-                      ])),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter your email",
-                      hintStyle: GoogleFonts.roboto(
-                          color: Colors.grey,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                      suffixIcon: Icon(
-                        Icons.email,
-                        size: 30,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    style: GoogleFonts.roboto(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                    keyboardType: TextInputType.number,
-                  ),
-                  SizedBox(height: 40),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Enter your password",
-                      hintStyle: GoogleFonts.roboto(
-                          color: Colors.grey,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            if (h1 == 0) {
-                              h1 = 1;
-                            } else {
-                              h1 = 0;
-                            }
-                          });
-                        },
-                        icon: Icon(
-                          h1 == 0 ? Icons.visibility_off : Icons.visibility,
-                          size: 30,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ),
-                    style: GoogleFonts.roboto(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                    ),
-                    obscureText: h1==0?true:false,
-                    obscuringCharacter: "*",
-                  ),
-                  SizedBox(height: 40),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Confirm your password",
+                      hintText: "Password",
                       hintStyle: GoogleFonts.roboto(
                           color: Colors.grey,
                           fontSize: 20,
@@ -213,10 +86,41 @@ class _LoginPageState extends State<LoginPage> {
                       fontWeight: FontWeight.bold,
                       fontSize: 20,
                     ),
-                    obscureText: h2==0?true:false,
+                    obscureText: h2 == 0 ? true : false,
                     obscuringCharacter: "*",
                   ),
                   SizedBox(height: 40),
+                  Container(
+                    width: 200,
+                    height: 70,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Icon(
+                            Icons.login,
+                            size: 30,
+                          ),
+                          Text(
+                            "Login",
+                            style: GoogleFonts.roboto(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xff715cf8),
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )
+                      ),
+                    ),
+                  )
                 ],
               ),
             )),
